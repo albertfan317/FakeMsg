62656e48cff048eaf4b02a1b74864dc5895f50bf
BuildTools <unconfigured@null.spigotmc.org>
2021-12-06 17:16:52 +0800

    initialcommit

--- main.js
+++ main.js
@@ -8,16 +8,10 @@ liff.init({
   // 寄送訊息
   liff.sendMessages([
     {
-      type: "text",
-      text: "You've successfully sent a message! Hooray!",
-    },
-  ])
-    .then(() => {
-      console.log('message sent');
-    })
-    .catch((err) => {
-      console.log('error', err);
-    });
+      type: 'text',
+      text: 'Hello, World!'
+    }
+  ]);
   
 }).catch(function(error) {
   console.log(error);
